256181ef6aa918236c66870420f2877a82b8ef2b
AdiSabban10 <adialon050@gmail.com>
2024-05-05 16:13:57 +0300

    Add bookService

---
+++ miss-book/services/book.service.js
@@ -0,0 +1,77 @@
+import { utilService } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const BOOK_KEY = 'bookDB'
+_createBooks()
+
+export const bookService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyBook,
+
+}
+// For Debug (easy access from console):
+window.bs = bookService
+
+function query(filterBy = {}) {
+    return storageService.query(BOOK_KEY)
+        .then(books => books)
+}
+
+function get(bookId) {
+    return storageService.get(BOOK_KEY, bookId)
+        .then(book => {
+            book = _setNextPrevBookId(book)
+            return book
+        })
+}
+
+function remove(bookId) {
+    return storageService.remove(BOOK_KEY, bookId)
+}
+
+function save(book) {
+    if (book.id) {
+        return storageService.put(BOOK_KEY, book)
+    } else {
+        return storageService.post(BOOK_KEY, book)
+    }
+}
+
+function getEmptyBook(title = '', listPrice = '') {
+    return { title, listPrice }
+}
+
+
+function _createBooks() {
+    let books = utilService.loadFromStorage(BOOK_KEY)
+    if (!books || !books.length) {
+        books = [
+            _createBook('Harry Potter and the sorcerers stone', 120),
+            _createBook('World Atlas', 300),
+            _createBook('Zorba the Greek', 87)
+        ]
+        
+        utilService.saveToStorage(BOOK_KEY, books)
+    }
+}
+
+function _createBook(title, listPrice) {
+    const book = getEmptyBook(title, listPrice)
+    book.id = utilService.makeId()
+    return book
+}
+
+function _setNextPrevBookId(book) {
+    return storageService.query(BOOK_KEY).then((books) => {
+        const bookIdx = books.findIndex((currBook) => currBook.id === book.id)
+        const nextBook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+        const prevBook = books[bookIdx - 1] ? books[bookIdx - 1] : books[books.length - 1]
+        book.nextBookId = nextBook.id
+        book.prevBookId = prevBook.id
+        return book
+    })
+}
+
