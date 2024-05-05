2b53afed34529a636f55710d030fa1becbf1c425
AdiSabban10 <adialon050@gmail.com>
2024-05-05 16:14:50 +0300

    Connect to bookService

--- miss-book/pages/BookIndex.jsx
+++ miss-book/pages/BookIndex.jsx
@@ -1,5 +1,19 @@
+const { useState, useEffect } = React
+
+import { bookService } from '../services/book.service.js'
+
 export function BookIndex () {
+
+    const [ books, setBooks ] = useState([])
+
+    useEffect(() => {
+        bookService.query()
+            .then(books => setBooks(books))
+
+    }, [])
+
     return <section className="book-index">
         <h1>Books</h1>
+        <pre>{JSON.stringify(books, null, 2)}</pre>
     </section>
 }
